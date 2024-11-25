7445f71aa8e1e93eba7c8229ebe0dfd7ef12628c
LucaCampusfp <lucacampusfp@gmail.com>
2024-11-25 15:01:30 +0100

    EXAMEN

---
+++ examen-luca-jin/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examen-luca-jin',
+  webDir: 'www'
+};
+
+export default config;
